f75c675e89062a2b5cd6fe4289e6f606a106e073
Sam Mumford <smmmfrd@gmail.com>
2022-08-17 13:22:44 -0700

    Implement Basic Cart Functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,27 @@
+import itemData from "../itemData"
+import { CartContext } from "../cartContext"
+import { useContext } from "react"
 export default function Cart(){
+    const {cart} = useContext(CartContext)
+    var total = 0;
+    function makeCartItems(){
+        var arr = [];
+        for(var key in cart){
+            const {id, name, price} = itemData.find(item => item.id === key);
+            const amount = price * cart[key];
+            total += amount;
+            arr.push(<div key={id}>
+                <p><strong>{name}</strong> Amount: {cart[key]} Cost:{price} Total:{amount}</p>
+            </div>);
+        }
+        return arr;
+    }
+    const cartElements = makeCartItems();
     return(
         <>
             <h1>Cart</h1>
+            {cartElements}
+            <h2>Grand Total: {total}</h2>
         </>
     )
 }
