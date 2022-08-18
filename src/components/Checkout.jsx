33fc48e2843327d2392e80e2d3cb1c6b3ea80a5a
Sam Mumford <smmmfrd@gmail.com>
2022-08-18 15:16:26 -0700

    Checkout Text

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,10 @@
 export default function Checkout(){
     return(
-        <>
+        <div className="checkout">
             <h1>Checkout</h1>
-        </>
+            <p>RMT, Real Money Trading, is paying real world money for in game item. It is a common service online, but most games will ban you for doing it. With Path of Exile, Grinding Gear Games will likely just delete your account.</p>
+            <p>In Path of Exile, there are a lot of youtube channels and websites that recommend extremely expensive builds which push unsuspecting players towards RMT sites.</p>
+            <p>Grinding Gear Games is also extremely good and knowing when someone is RMTing as they keep extensive logs of player trades. When a player receives extremely rare items in a trade while offering nothing, it's pretty obvious.</p>
+        </div>
     )
 }
